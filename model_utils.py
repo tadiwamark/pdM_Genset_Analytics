2271fb825898c863b9f91a58add19bdeda6874b2
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-04-14 13:52:49 +0200

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -142,6 +142,8 @@ def create_sequences(data, seq_length):
 def load_model_from_github(url):
     filename = url.split('/')[-1]
     urllib.request.urlretrieve(url, filename)
-    loaded_model = tf.keras.models.load_model(filename)
+    custom_objects = {'TransformerEncoderBlock': TransformerEncoderBlock}
+    loaded_model = tf.keras.models.load_model(filename, custom_objects=custom_objects)
+
     return loaded_model
 
