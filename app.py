1d2a4856c70e30b6d2a58813f823175379b1fd3c
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-04-22 14:01:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def main():
 
       simulated_data_df
       simulated_data_df['Load_Factor'] = simulated_data_df['AverageCurrent(A)'] / simulated_data_df['Phase1Current(A)'].max()
-      simulated_data_df['Temp_Gradient'] = simulated_data_df['ExhaustTemp(°C)] - simulated_data_df['CoolantTemp( °C)']
+      simulated_data_df['Temp_Gradient'] = simulated_data_df['ExhaustTemp(°C)'] - simulated_data_df['CoolantTemp( °C)']
       simulated_data_df['Pressure_Ratio'] = simulated_data_df['inLetPressure(KPa)'] / simulated_data_df['outLetPressure(KPa)']
       simulated_data_df['Imbalance_Current'] = simulated_data_df[['Phase1Current(A)', 'Phase2Current(A)', 'Phase3Current(A)']].std(axis=1)
       simulated_data_df['Power_Factor_Deviation'] = 1 - simulated_data_df['PowerFactor'].abs()
