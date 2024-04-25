a382b819d8c0298406832319bbac7b71984e1251
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-04-25 12:25:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from datetime import datetime, timedelta
 from generator_script import generate_continuous_data
 from model_utils import detect_anomalies, generate_diagnosis_and_recommendation, generate_prompts_from_anomalies, inverse_transform, create_sequences, load_model_from_github, download_and_load_scaler
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 
