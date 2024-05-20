29c149ec427d96c33d917503f3bab1e0fc27df71
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-05-20 14:50:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,15 +88,15 @@ def main():
 
           
           # Graphical display
-          fig, ax = plt.subplots(figsize=(15, 8))
-          ax.plot(simulated_data_df.index, simulated_data_df['AverageCurrent(A)'].rolling(window=10).mean(), label='Average Current (A)', color='blue')
-          ax.plot(simulated_data_df.index, simulated_data_df['Phase1Current(A)'].rolling(window=10).mean(), label='Phase 1 Current (A)', color='red', linestyle='--')
-          ax.plot(simulated_data_df.index, simulated_data_df['Phase2Current(A)'].rolling(window=10).mean(), label='Phase 2 Current (A)', color='green', linestyle='--')
-          ax.plot(simulated_data_df.index, simulated_data_df['Phase3Current(A)'].rolling(window=10).mean(), label='Phase 3 Current (A)', color='purple', linestyle='--')
-          ax.set_xlabel('Time')
-          ax.set_ylabel('Current (A)')
-          ax.legend()
-          graph_placeholder.pyplot(fig)
+          fig1, ax1 = plt.subplots(figsize=(15, 8))
+          ax1.plot(simulated_data_df['Timestamp'], simulated_data_df['AverageCurrent(A)'].rolling(window=10).mean(), label='Average Current (A)', color='blue')
+          ax1.plot(simulated_data_df['Timestamp'], simulated_data_df['Phase1Current(A)'].rolling(window=10).mean(), label='Phase 1 Current (A)', color='red', linestyle='--')
+          ax1.plot(simulated_data_df['Timestamp'], simulated_data_df['Phase2Current(A)'].rolling(window=10).mean(), label='Phase 2 Current (A)', color='green', linestyle='--')
+          ax1.plot(simulated_data_df['Timestamp'], simulated_data_df['Phase3Current(A)'].rolling(window=10).mean(), label='Phase 3 Current (A)', color='purple', linestyle='--')
+          ax1.set_xlabel('Time')
+          ax1.set_ylabel('Current (A)')
+          ax1.legend()
+          graph_placeholder.pyplot(fig1)
 
           time.sleep(5) 
 
