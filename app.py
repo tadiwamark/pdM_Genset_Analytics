6d69e7c42d50ec63d0d3e95c7c5031c6e6f6b276
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-05-20 10:39:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,11 +104,11 @@ def main():
                 scaled_data_seq = create_sequences(scaled_data_df, 10)
 
                 # Graphical display
-                fig, ax = plt.subplots(figsize=(15, 8))
-                ax.plot(simulated_data_df.index, simulated_data_df['AverageCurrent(A)'].rolling(window=10).mean(), label='Average Current (A)', color='blue')
-                ax.plot(simulated_data_df.index, simulated_data_df['Phase1Current(A)'].rolling(window=10).mean(), label='Phase 1 Current (A)', color='red', linestyle='--')
-                ax.plot(simulated_data_df.index, simulated_data_df['Phase2Current(A)'].rolling(window=10).mean(), label='Phase 2 Current (A)', color='green', linestyle='--')
-                ax.plot(simulated_data_df.index, simulated_data_df['Phase3Current(A)'].rolling(window=10).mean(), label='Phase 3 Current (A)', color='purple', linestyle='--')
+                fig, ax = plt.subplots()
+                ax.plot(simulated_data_df.index, simulated_data_df['AverageCurrent(A)'], label='Average Current (A)', color='blue')
+                ax.plot(simulated_data_df.index, simulated_data_df['Phase1Current(A)'], label='Phase 1 Current (A)', color='red', linestyle='--')
+                ax.plot(simulated_data_df.index, simulated_data_df['Phase2Current(A)'], label='Phase 2 Current (A)', color='green', linestyle='--')
+                ax.plot(simulated_data_df.index, simulated_data_df['Phase3Current(A)'], label='Phase 3 Current (A)', color='purple', linestyle='--')
                 ax.set_xlabel('Time')
                 ax.set_ylabel('Current (A)')
                 ax.legend()
@@ -116,6 +116,7 @@ def main():
 
                 for _, row in simulated_data_df.iterrows():
                     # Display simulated data
+                    data_placeholder.dataframe(row.drop('Anomaly_Type').to_frame().T)
 
                     # Detect anomalies in the simulated data
                     optimal_threshold = 0.7
@@ -143,8 +144,6 @@ def main():
                         send_email("Generator Anomaly Alert", diagnosis)
 
                         time.sleep(60)
-
-        status_placeholder.success("Generator is currently ON.")
 
     else:
         st.write("Generator is currently OFF. Use the sidebar to start the generator.")
