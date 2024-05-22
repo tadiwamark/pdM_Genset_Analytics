c2388b93c521ae91c1220b8de3354d5fe11d2e8b
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-05-22 08:50:52 +0200

    Update generator_script.py

--- generator_script.py
+++ generator_script.py
@@ -29,23 +29,24 @@ def generate_continuous_data(start_time):
         current_time = start_time + timedelta(seconds=5)
         data_records = []
         simulated_data = {
-            'Time': [current_time.strftime('%m/%d/%Y %H:%M')],
-            'AverageCurrent(A)': [generate_parameter_value(150, 650)],
-            'Phase1Current(A)': [generate_parameter_value(150, 650)],
-            'Phase2Current(A)': [generate_parameter_value(150, 650)],
-            'Phase3Current(A)': [generate_parameter_value(150, 650)],
-            'ExhaustTemp(°C)': [generate_parameter_value(500, 750)],
-            'inLetPressure(KPa)': [generate_parameter_value(25, 100)],
-            'outLetPressure(KPa)': [generate_parameter_value(20, 90)],
-            'OutLetAirTemp(°C)' : [generate_parameter_value(20, 41)],
-            'CoolantTemp( °C)' : [generate_parameter_value(30, 95)],
-            'OilPressure(KPa)' : [generate_parameter_value(200, 500)],
-            'PowerFactor' : [generate_parameter_value(0.8, 1.2)],
-            'Speed(Rpm)' : [generate_parameter_value(1200, 1700)],
-            'AmbientTemp( °C)': [generate_parameter_value(25, 35)],
-            'FuelLevel(Ltrs)' : [generate_parameter_value(1340,1500)],
-            'Freq(Hz)' : [generate_parameter_value(0, 60)],
+            'Timestamp': current_time,
+            'AverageCurrent(A)': generate_parameter_value(150, 650),
+            'Phase1Current(A)': generate_parameter_value(150, 650),
+            'Phase2Current(A)': generate_parameter_value(150, 650),
+            'Phase3Current(A)': generate_parameter_value(150, 650),
+            'ExhaustTemp(°C)': generate_parameter_value(500, 750),
+            'inLetPressure(KPa)': generate_parameter_value(25, 100),
+            'outLetPressure(KPa)': generate_parameter_value(20, 90),
+            'OutLetAirTemp(°C)': generate_parameter_value(20, 41),
+            'CoolantTemp(°C)': generate_parameter_value(30, 95),
+            'OilPressure(KPa)': generate_parameter_value(200, 500),
+            'PowerFactor': generate_parameter_value(0.8, 1.2),
+            'Speed(Rpm)': generate_parameter_value(1200, 1700),
+            'AmbientTemp(°C)': generate_parameter_value(25, 35),
+            'FuelLevel(Ltrs)': generate_parameter_value(1340, 1500),
+            'Freq(Hz)': generate_parameter_value(0, 60)
         }
+        
         simulated_data, anomaly_type = simulate_anomalies(simulated_data)
         simulated_data['Anomaly_Type'] = anomaly_type
         data_records.append(simulated_data)
@@ -53,7 +54,7 @@ def generate_continuous_data(start_time):
 
         print(f"Generated {len(data_records)} records.")
 
-        yield pd.DataFrame([simulated_df])
+        yield simulated_df
 
         start_time = current_time
-        time.sleep(5) 
+        time.sleep(5)
