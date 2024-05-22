046374c49a2472ba876618b975a8d8b60e5f7e86
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-05-22 12:44:45 +0200

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -169,6 +169,8 @@ def send_email(subject, body):
 
     message.attach(MIMEText(body, "plain"))
 
-    with smtplib.SMTP_SSL("smtp-mail.outlook.com", 465) as server:
+    with smtplib.SMTP("smtp-mail.outlook.com", 587) as server:
+        server.starttls()
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
+
