c70743fbfecbfb59521127dfcc7c4d3b124b9fae
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2024-06-06 10:49:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -145,10 +145,10 @@ def main():
 
 
 
-                    if len(accumulated_data) >= 60:  # Process data every 60 records (5 minutes assuming 5 sec interval)
+                    if len(accumulated_data) >= 15:  # Process data every 60 records (5 minutes assuming 5 sec interval)
                         optimal_threshold = 0.7
                         features = scaled_data.shape[1]
-                        anomalies, real_predictions, fake_predictions = detect_anomalies(generator_model, discriminator_model, scaled_data_seq, features, numeric_columns)
+                        anomalies, real_predictions, fake_predictions = detect_anomalies(generator_model, discriminator_model, scaled_data_seq, features)
                         real_predictions = discriminator_model.predict(scaled_data_seq)
                         
                         anomalies_indices = np.where(real_predictions < optimal_threshold)[0]
